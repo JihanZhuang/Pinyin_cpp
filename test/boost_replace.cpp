1190575254094a97f94cdbcf70c3600f9b079afe
JihanZhuang <zhuanghan1103@163.com>
2016-10-21 15:03:54 +0800

    alnum is not what the code expected

--- test/boost_replace.cpp
+++ test/boost_replace.cpp
@@ -12,16 +12,20 @@
 using namespace std;
 
 int main(){
-  std::string str = "带着希望去旅行，比到达终点更美好,it is very beautiful,也错!Quick brown FOX";
+  std::string str = "112带着希望去旅行，比到达终点更美好,it is very beautiful,也错!Quick brown FOX,üé";
   std::string newtext = "\t$&";
-  boost::regex re("[a-z0-9_-]+",boost::regex::perl|boost::regex::icase);
-  boost::regex a_re("[^\u4e00-\u9fa5]",boost::regex::perl|boost::regex::icase);
+  //boost::regex re("[a-z0-9_-]+",boost::regex::perl|boost::regex::icase);
+  //boost::regex a_re("[^\u4e00-\u9fa5]|[[:space:]]|[[:punct:]]",boost::regex::perl|boost::regex::icase);
+  boost::regex a_re("[[:alnum:]]",boost::regex::perl|boost::regex::icase);
+  //boost::regex b_re("[[:space:]]",boost::regex::perl|boost::regex::icase);
 
   std::cout << str << std::endl;
 
-  std::string result = boost::regex_replace(str, re, newtext);
+  //std::string result = boost::regex_replace(str, re, newtext);
+  //std::cout << result << std::endl;
+  std::string result = boost::regex_replace(str, a_re, newtext);
   std::cout << result << std::endl;
-  result = boost::regex_replace(str, a_re, newtext);
-  std::cout << result << std::endl;
+  //result = boost::regex_replace(str, b_re, newtext);
+  //std::cout << result << std::endl;
 }
 
