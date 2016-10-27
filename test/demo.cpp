415cde94927f1186ae3016c8a4bb9eccb3fa222c
JihanZhuang <zhuanghan1103@163.com>
2016-10-27 08:49:52 +0800

    test

--- test/demo.cpp
+++ test/demo.cpp
@@ -9,7 +9,7 @@ const char* const HMM_PATH = "./dict/hmm_model.utf8";
 const char* const USER_DICT_PATH = "./dict/user.dict.utf8";
 const char* const IDF_PATH = "./dict/idf.utf8";
 const char* const STOP_WORD_PATH = "./dict/stop_words.utf8";
-
+ 
 int main(int argc, char** argv) {
   cppjieba::Jieba jieba(DICT_PATH,
         HMM_PATH,
